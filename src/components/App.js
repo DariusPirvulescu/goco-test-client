538d8776b9b24b727b683123584b9b9106d74c5f
Darius Pirvulescu <organicdarius@gmail.com>
2021-01-28 12:30:20 +0200

    Add basic routing, Create Homepage component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Homepage from "./Homepage";
 import Login from "./Login";
 import Register from "./Register";
 
 const App = () => {
   return (
-    <> 
+    <Router>
       <h1>App</h1>
-      <Login />
-      <Register />
-    </>
+      <Switch>
+      <Route exact path='/' component={Homepage} />
+      <Route path='/login' component={Login} />
+      <Route path='/register' component={Register} />
+      </Switch>
+    </Router>
   )
 }
 
