27a2272cfabb49ed2b1b46d232f8dea910c9f027
Darius Pirvulescu <organicdarius@gmail.com>
2021-01-28 12:22:02 +0200

    Create Login and Register components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import Login from "./Login";
+import Register from "./Register";
+
 const App = () => {
-  return <h1>App</h1>
+  return (
+    <> 
+      <h1>App</h1>
+      <Login />
+      <Register />
+    </>
+  )
 }
 
 export default App;
